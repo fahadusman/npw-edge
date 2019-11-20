7967d459f8c9bd17e360eeb10cabdbccf1bfff37
Fahad Usman <fahad.usman@intechww.com>
2019-11-20 15:00:23 +0500

    communicator.h: Add enqueueMessage function
    Enqueue messages for transmission by specific communicator
    of derived class.
    
    Signed-off-by: Fahad Usman <fahad.usman@intechww.com>

--- include/communicator.h
+++ include/communicator.h
@@ -9,6 +9,7 @@
 #define INCLUDE_COMMUNICATOR_H_
 #include "CommDataBuffer.h"
 #include <map>
+#include "glog/logging.h"
 
 class communicator{
 protected:
@@ -16,6 +17,11 @@ protected:
 public:
 	communicator(){}
 	virtual void sendMessage(const char * message, const unsigned int length) = 0;
+	virtual int enqueueMessage(CommDataBuffer * buff){
+	    auto ret = transmitQueue.insert(std::pair<uint64_t, CommDataBuffer * >(buff->getTimestamp(), buff));
+	    LOG(INFO) << "insert in transmission queue with t: " << buff->getTimestamp() << "\t Queue size: " << transmitQueue.size();
+	    return 0;
+	}
 	virtual ~communicator(){}
 	virtual void connect() = 0;
 	virtual void disconnect() = 0;
