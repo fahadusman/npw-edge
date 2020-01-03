8879e527bad40aacb690232aa84b1346af4033c7
Fahad Usman <fahad.usman@intechww.com>
2020-01-03 12:24:12 +0500

    StationEdgeDevice: Process incoming commands
    https://intechww.atlassian.net/browse/PAR-69
    
    Signed-off-by: Fahad Usman <fahad.usman@intechww.com>

--- include/StationEdgeDevice.h
+++ include/StationEdgeDevice.h
@@ -17,9 +17,11 @@ public:
     StationEdgeDevice();
     virtual ~StationEdgeDevice();
     void processIncomingCommand(CommandMsg * incomingCommand);
+    void setHeartbeatInterval(int32_t hb);
 protected:
     std::list<Sensor *> sensorsList;  //List of PTs and TTs
     communicator * commPtr;
+    uint32_t heartbeatInterval; //Heartbeat interval for edge device in MS
 };
 
 #endif /* INCLUDE_STATIONEDGEDEVICE_H_ */
--- src/StationEdgeDevice.cpp
+++ src/StationEdgeDevice.cpp
@@ -9,6 +9,8 @@
 
 #include <glog/logging.h>
 
+#include "DevConfig.h"
+
 StationEdgeDevice::StationEdgeDevice() {
     LOG(INFO) << "StationEdgeDevice constructor";
     commPtr = new MqttCommunicator(this);
@@ -17,15 +19,56 @@ StationEdgeDevice::StationEdgeDevice() {
     PressureSensor * sensorPtr = new PressureSensor("/dev/ttyUSB0", commPtr);
     sensorPtr->startNpwThread();
     sensorsList.push_back(sensorPtr);
-
+    heartbeatInterval = kDcHeartbeatInterval.def;
 }
 
 void StationEdgeDevice::processIncomingCommand(CommandMsg * incomingCommand){
-    for (Sensor * sensorPtr : sensorsList) {
-        sensorPtr->enqueueCommand(incomingCommand);
+    switch (incomingCommand->getCommand()){
+    case UNINITIALIZED_CMD:
+        LOG(WARNING) << "Uninitialized command received.";
+        delete incomingCommand;
+        break;
+    case REBOOT_TIME:
+        //TODO: Implement reboot mechanism
+        delete incomingCommand;
+        break;
+    case HEARTBEAT_INTERVAL:
+        setHeartbeatInterval(incomingCommand->getData());
+        delete incomingCommand;
+        break;
+    case MAX_TIME_PERIODIC:
+    case MIN_TIME_PERIODIC:
+    case ON_CHANG_THSH_PT:
+    case NPW_EXP_TIME:
+    case TEST_FLAG:
+
+    case SAMPL_FREQ_NPW:
+    case NUM_SAMPLES_1_AVG:
+    case NUM_SAMPLES_2_AVG:
+    case START_SAMPLE_2_AVG:
+    case NPW_THR_PT1:
+    case NPW_THR_PT2:
+    case NPW_THR_PT3:
+    case NPW_THR_PT4:
+        for (Sensor * sensorPtr : sensorsList) {
+            sensorPtr->enqueueCommand(incomingCommand);
+        }
+        break;
+    case INVALID_COMMAND:
+    default:
+        LOG(WARNING) << "Unhandled command received.";
+        delete incomingCommand;
+        incomingCommand = NULL;
     }
 }
 
+void StationEdgeDevice::setHeartbeatInterval(int32_t hb) {
+    if (hb > kDcHeartbeatInterval.min and hb < kDcHeartbeatInterval.max) {
+        heartbeatInterval = hb * 1000; //convert it from seconds to milliseconds
+    } else {
+        LOG(WARNING) << "HEARTBEAT_INTERVAL value out of range";
+    }
+}
 StationEdgeDevice::~StationEdgeDevice() {
     // TODO Auto-generated destructor stub
     for (Sensor * sensorPtr : sensorsList) {
