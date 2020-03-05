2e9e77145425bb793d3bfbd0382ffe2426254c96
Fahad Usman <fahad.usman@intechww.com>
2020-03-05 16:26:27 +0500

    communicator: Also log buffer id when inserting buffer in transmit queue

--- src/communicator.cpp
+++ src/communicator.cpp
@@ -10,7 +10,10 @@ communicator::communicator(EdgeDevice * d) {
 }
 
 int communicator::enqueueMessage(CommDataBuffer * buff){
-    LOG(INFO) << "Going to insert in transmission queue with t: " << buff->getTimestamp() << "\t Queue size: " << transmitQueue.size();
+    LOG(INFO) << "Going to insert in transmission queue with t: "
+            << buff->getTimestamp() << ", id: " << buff->getBufferId()
+            << "\t Queue size: "  << transmitQueue.size();
+
     std::lock_guard<std::mutex> guard(transmitQueueMutex);
     transmitQueue[buff->getBufferId()] = buff;
     return 0;
