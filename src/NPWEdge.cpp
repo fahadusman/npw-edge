caa85ce4c4fa773a641cf2e77f4db94464662524
Fahad Usman <fahad.usman@intechww.com>
2020-04-13 14:31:21 +0500

    merge commit

--- src/NPWEdge.cpp
+++ src/NPWEdge.cpp
@@ -11,13 +11,27 @@ int main(int argc, char **argv) {
     google::InitGoogleLogging(argv[0]);
     LOG(INFO) << "starting LDS app, argc = " << argc;
 
+
     Role edgeDeviceRole = gatewayEdgeDevice;
     int edgeDeviceId = 1;
     edgeDeviceRole = bvEdgeDevice; edgeDeviceId = 2;
+//    edgeDeviceRole = stationEdgeDevice; edgeDeviceId = 3;
 
     EdgeDevice edgeDevice(edgeDeviceId, edgeDeviceRole);
     communicator * commPtr = nullptr;
 
+//    commPtr = new MqttCommunicator(&edgeDevice);
+//    commPtr->connect();
+//    commPtr->subscribe();
+//    edgeDevice.setCommunicator(commPtr);
+//    return 0;
+
+//    TemperatureSensor tempSensor("/dev/pts/2", commPtr, &edgeDevice, "TT1");
+//    tempSensor.startThread();
+//    edgeDevice.runForever();
+//
+//    return 0;
+    
     if (edgeDeviceRole == stationEdgeDevice) {
         commPtr = new MqttCommunicator(&edgeDevice);
     } else if (edgeDeviceRole == bvEdgeDevice) {
@@ -29,6 +43,8 @@ int main(int argc, char **argv) {
         RadioCommunicator * modbusMasterPtr =
                 new RadioCommunicator(&edgeDevice, 1, modbusMaster, "/dev/pts/0");
         modbusMasterPtr->addModbusSlave(1);
+//        modbusMasterPtr->addModbusSlave(2);
+//        modbusMasterPtr->addModbusSlave(3);
 
         edgeDevice.setModbusMaster(modbusMasterPtr);
         modbusMasterPtr->connect();
@@ -37,6 +53,10 @@ int main(int argc, char **argv) {
         Sensor *sensorPtr = new PressureSensor("/dev/ttyUSB0", commPtr,
                 &edgeDevice, "PT1");
         edgeDevice.addSensor(sensorPtr);
+
+//        sensorPtr = new TemperatureSensor("/dev/pts/2", commPtr,
+//                        &edgeDevice, "TT1");
+//                edgeDevice.addSensor(sensorPtr);
     }
 
     commPtr->connect();
