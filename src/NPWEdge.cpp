aa8b214cce0e16ba2b9d5768efe2a843e5bab984
Fahad Usman <fahad.usman@intechww.com>
2020-09-18 16:30:57 +0500

    NPWEdge: update name of config file
    Signed-off-by: Fahad Usman <fahad.usman@intechww.com>

--- src/NPWEdge.cpp
+++ src/NPWEdge.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv) {
     google::InitGoogleLogging(argv[0]);
     LOG(INFO) << "starting LDS app, argc = " << argc;
 
-    EdgeDevice e("config_gateway.json");
+    EdgeDevice e("config.json");
     e.runForever();
 
     return 0;
